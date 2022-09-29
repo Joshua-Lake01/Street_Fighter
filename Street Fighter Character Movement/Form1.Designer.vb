817dcb72e03122db054040eb61ebe2ad1f664c56
Joshua Lake <110567609+Joshua-Lake01@users.noreply.github.com>
2022-09-29 19:48:01 +0100

    Add files via upload
    This is not finished but helped me and may help others to understand the logic in 2D character collision and movement. This is all integrated into a windows form and in Visual Basic.

---
+++ Street Fighter Character Movement/Form1.Designer.vb
@@ -0,0 +1,341 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.C1Bop1 = New System.Windows.Forms.Timer(Me.components)
+        Me.C1Bop2 = New System.Windows.Forms.Timer(Me.components)
+        Me.C1Foward = New System.Windows.Forms.Timer(Me.components)
+        Me.C1Back = New System.Windows.Forms.Timer(Me.components)
+        Me.C1Jump = New System.Windows.Forms.Timer(Me.components)
+        Me.Gravity = New System.Windows.Forms.Timer(Me.components)
+        Me.C1B1 = New System.Windows.Forms.Timer(Me.components)
+        Me.C1B2 = New System.Windows.Forms.Timer(Me.components)
+        Me.C2B1 = New System.Windows.Forms.Timer(Me.components)
+        Me.C2B2 = New System.Windows.Forms.Timer(Me.components)
+        Me.C2B3 = New System.Windows.Forms.Timer(Me.components)
+        Me.C2B4 = New System.Windows.Forms.Timer(Me.components)
+        Me.C2Foward = New System.Windows.Forms.Timer(Me.components)
+        Me.C2Back = New System.Windows.Forms.Timer(Me.components)
+        Me.C2Jump = New System.Windows.Forms.Timer(Me.components)
+        Me.C2Gravity = New System.Windows.Forms.Timer(Me.components)
+        Me.C1Punch1 = New System.Windows.Forms.Timer(Me.components)
+        Me.C1Punch2 = New System.Windows.Forms.Timer(Me.components)
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.ProgressBar2 = New System.Windows.Forms.ProgressBar()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.C1Punch = New System.Windows.Forms.PictureBox()
+        Me.C2P1 = New System.Windows.Forms.PictureBox()
+        Me.C2P2 = New System.Windows.Forms.PictureBox()
+        Me.C1P4 = New System.Windows.Forms.PictureBox()
+        Me.C1P3 = New System.Windows.Forms.PictureBox()
+        Me.C1P2 = New System.Windows.Forms.PictureBox()
+        Me.C1P1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.C1Damage1 = New System.Windows.Forms.Timer(Me.components)
+        Me.C1Damage2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C1Punch, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C2P1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C2P2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C1P4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C1P3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C1P2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.C1P1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'C1Bop1
+        '
+        Me.C1Bop1.Interval = 400
+        '
+        'C1Bop2
+        '
+        Me.C1Bop2.Interval = 400
+        '
+        'C1Foward
+        '
+        '
+        'C1Back
+        '
+        '
+        'C1Jump
+        '
+        '
+        'Gravity
+        '
+        '
+        'C1B1
+        '
+        Me.C1B1.Interval = 400
+        '
+        'C1B2
+        '
+        Me.C1B2.Interval = 400
+        '
+        'C2B1
+        '
+        Me.C2B1.Enabled = True
+        Me.C2B1.Interval = 400
+        '
+        'C2B2
+        '
+        Me.C2B2.Interval = 400
+        '
+        'C2B3
+        '
+        Me.C2B3.Interval = 400
+        '
+        'C2B4
+        '
+        Me.C2B4.Interval = 400
+        '
+        'C2Foward
+        '
+        '
+        'C2Back
+        '
+        '
+        'C1Punch1
+        '
+        '
+        'C1Punch2
+        '
+        Me.C1Punch2.Interval = 300
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 120)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(1258, 52)
+        Me.ProgressBar1.TabIndex = 7
+        '
+        'ProgressBar2
+        '
+        Me.ProgressBar2.Location = New System.Drawing.Point(1466, 120)
+        Me.ProgressBar2.Name = "ProgressBar2"
+        Me.ProgressBar2.Size = New System.Drawing.Size(1258, 52)
+        Me.ProgressBar2.TabIndex = 8
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Tan
+        Me.PictureBox1.BackgroundImage = Global.Street_Fighter_Character_Movement.My.Resources.Resources.Vs3
+        Me.PictureBox1.Location = New System.Drawing.Point(1276, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(184, 160)
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
+        '
+        'C1Punch
+        '
+        Me.C1Punch.BackColor = System.Drawing.Color.Transparent
+        Me.C1Punch.BackgroundImage = Global.Street_Fighter_Character_Movement.My.Resources.Resources.Punch
+        Me.C1Punch.Location = New System.Drawing.Point(2112, 1249)
+        Me.C1Punch.Name = "C1Punch"
+        Me.C1Punch.Size = New System.Drawing.Size(474, 376)
+        Me.C1Punch.TabIndex = 6
+        Me.C1Punch.TabStop = False
+        Me.C1Punch.Visible = False
+        '
+        'C2P1
+        '
+        Me.C2P1.BackColor = System.Drawing.Color.Transparent
+        Me.C2P1.BackgroundImage = Global.Street_Fighter_Character_Movement.My.Resources.Resources.B13
+        Me.C2P1.Location = New System.Drawing.Point(115, 1099)
+        Me.C2P1.Name = "C2P1"
+        Me.C2P1.Size = New System.Drawing.Size(353, 526)
+        Me.C2P1.TabIndex = 5
+        Me.C2P1.TabStop = False
+        '
+        'C2P2
+        '
+        Me.C2P2.BackColor = System.Drawing.Color.Transparent
+        Me.C2P2.BackgroundImage = Global.Street_Fighter_Character_Movement.My.Resources.Resources.B22
+        Me.C2P2.Location = New System.Drawing.Point(115, 1099)
+        Me.C2P2.Name = "C2P2"
+        Me.C2P2.Size = New System.Drawing.Size(353, 526)
+        Me.C2P2.TabIndex = 4
+        Me.C2P2.TabStop = False
+        '
+        'C1P4
+        '
+        Me.C1P4.BackColor = System.Drawing.Color.Transparent
+        Me.C1P4.BackgroundImage = Global.Street_Fighter_Character_Movement.My.Resources.Resources.B2_Back
+        Me.C1P4.Location = New System.Drawing.Point(2233, 1099)
+        Me.C1P4.Name = "C1P4"
+        Me.C1P4.Size = New System.Drawing.Size(353, 526)
+        Me.C1P4.TabIndex = 3
+        Me.C1P4.TabStop = False
+        Me.C1P4.Visible = False
+        '
+        'C1P3
+        '
+        Me.C1P3.BackColor = System.Drawing.Color.Transparent
+        Me.C1P3.BackgroundImage = Global.Street_Fighter_Character_Movement.My.Resources.Resources.B1_Back
+        Me.C1P3.Location = New System.Drawing.Point(2233, 1099)
+        Me.C1P3.Name = "C1P3"
+        Me.C1P3.Size = New System.Drawing.Size(353, 526)
+        Me.C1P3.TabIndex = 2
+        Me.C1P3.TabStop = False
+        Me.C1P3.Visible = False
+        '
+        'C1P2
+        '
+        Me.C1P2.BackColor = System.Drawing.Color.Transparent
+        Me.C1P2.BackgroundImage = Global.Street_Fighter_Character_Movement.My.Resources.Resources.B2
+        Me.C1P2.Location = New System.Drawing.Point(2233, 1099)
+        Me.C1P2.Name = "C1P2"
+        Me.C1P2.Size = New System.Drawing.Size(353, 526)
+        Me.C1P2.TabIndex = 1
+        Me.C1P2.TabStop = False
+        Me.C1P2.Visible = False
+        '
+        'C1P1
+        '
+        Me.C1P1.BackColor = System.Drawing.Color.Transparent
+        Me.C1P1.BackgroundImage = Global.Street_Fighter_Character_Movement.My.Resources.Resources.B1
+        Me.C1P1.Location = New System.Drawing.Point(2233, 1099)
+        Me.C1P1.Name = "C1P1"
+        Me.C1P1.Size = New System.Drawing.Size(353, 526)
+        Me.C1P1.TabIndex = 0
+        Me.C1P1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Bahnschrift SemiBold SemiConden", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(473, 2)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(341, 115)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Player 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Bahnschrift SemiBold SemiConden", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(1928, 2)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(356, 115)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Player 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe Script", 13.875!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(87, 883)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(118, 58)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "- 10"
+        '
+        'C1Damage1
+        '
+        '
+        'C1Damage2
+        '
+        Me.C1Damage2.Interval = 600
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft JhengHei UI", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(1142, 780)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 122)
+        Me.Label4.TabIndex = 13
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Tan
+        Me.ClientSize = New System.Drawing.Size(2740, 1707)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ProgressBar2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.C1Punch)
+        Me.Controls.Add(Me.C2P1)
+        Me.Controls.Add(Me.C2P2)
+        Me.Controls.Add(Me.C1P4)
+        Me.Controls.Add(Me.C1P3)
+        Me.Controls.Add(Me.C1P2)
+        Me.Controls.Add(Me.C1P1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C1Punch, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C2P1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C2P2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C1P4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C1P3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C1P2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.C1P1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents C1P1 As PictureBox
+    Friend WithEvents C1Bop1 As Timer
+    Friend WithEvents C1Bop2 As Timer
+    Friend WithEvents C1P2 As PictureBox
+    Friend WithEvents C1Foward As Timer
+    Friend WithEvents C1Back As Timer
+    Friend WithEvents C1Jump As Timer
+    Friend WithEvents Gravity As Timer
+    Friend WithEvents C1P3 As PictureBox
+    Friend WithEvents C1P4 As PictureBox
+    Friend WithEvents C1B1 As Timer
+    Friend WithEvents C1B2 As Timer
+    Friend WithEvents C2P2 As PictureBox
+    Friend WithEvents C2P1 As PictureBox
+    Friend WithEvents C2B1 As Timer
+    Friend WithEvents C2B2 As Timer
+    Friend WithEvents C2B3 As Timer
+    Friend WithEvents C2B4 As Timer
+    Friend WithEvents C2Foward As Timer
+    Friend WithEvents C2Back As Timer
+    Friend WithEvents C2Jump As Timer
+    Friend WithEvents C2Gravity As Timer
+    Friend WithEvents C1Punch1 As Timer
+    Friend WithEvents C1Punch As PictureBox
+    Friend WithEvents C1Punch2 As Timer
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents ProgressBar2 As ProgressBar
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents C1Damage1 As Timer
+    Friend WithEvents C1Damage2 As Timer
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Street Fighter Character Movement/Form1.vb
@@ -0,0 +1,299 @@
+﻿Public Class Form1
+    Dim BlnJump As Boolean = True
+    Dim pressedD As Boolean = False
+    Dim pressedA As Boolean = False
+    Sub mydelay()
+
+        Dim icount As Integer = 1
+        For icount = 1 To 1000000000
+            icount = icount + 1
+        Next
+
+
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'character 1
+        C1P1.Visible = True
+        C1P2.Visible = False
+        C1Bop1.Enabled = True
+        C1Bop2.Enabled = False
+        C1B1.Enabled = False
+        C1B2.Enabled = False
+        C1P3.Visible = False
+        C1P4.Visible = False
+
+        'character 2
+        C2P1.Visible = True
+        C2P2.Visible = False
+        ProgressBar1.Value = 100
+        ProgressBar2.Value = 100
+
+        Label3.Visible = False
+        Label4.Visible = False
+
+
+    End Sub
+
+
+    'character 1
+    Private Sub C1Foward_Tick(sender As Object, e As EventArgs) Handles C1Foward.Tick
+
+        If C1P1.Left > Me.Width - 200 Then
+            C1Foward.Enabled = False
+        Else
+            C1P1.Left = C1P1.Left + 50
+            C1P2.Left = C1P1.Left
+            C1P3.Left = C1P1.Left
+            C1P4.Left = C1P1.Left
+            Label3.Left = C1P1.Left - 50
+            Label3.Top = C1P1.Top - 70
+            C1Punch.Left = C1P1.Left
+            C1Foward.Enabled = False
+        End If
+
+
+
+
+    End Sub
+
+    Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+        If e.KeyChar = "d" Then
+            C1Foward.Enabled = True
+            If pressedD = False Then
+                pressedD = True
+                pressedA = False
+                C1B1.Enabled = True
+                C1B2.Enabled = False
+                C1Bop1.Enabled = False
+                C1Bop2.Enabled = False
+            End If
+
+        End If
+        If e.KeyChar = "a" Then
+            C1Back.Enabled = True
+            If pressedA = False Then
+                pressedA = True
+                pressedD = False
+                C1B1.Enabled = False
+                C1B2.Enabled = False
+                C1Bop1.Enabled = True
+                C1Bop2.Enabled = False
+
+            End If
+        End If
+
+        If e.KeyChar = "w" Then
+            If BlnJump = True Then
+                C1Jump.Enabled = True
+                BlnJump = False
+            End If
+
+        End If
+
+        If e.KeyChar = "c" Then
+            C1Punch1.Enabled = True
+            If C1Punch.Enabled = True Then
+                If C2P1.Left + C2P1.Width + 10 > C1P1.Left Then
+                    If ProgressBar1.Value = 10 Then
+                        Label4.Text = "Game Over"
+                        Label4.Visible = True
+                        C1Bop1.Enabled = False
+                        C1Bop2.Enabled = False
+                        C1B1.Enabled = False
+                        C1B2.Enabled = False
+                        C1Foward.Enabled = False
+                        C1Back.Enabled = False
+                        C1Punch.Enabled = False
+                        C1Punch1.Enabled = False
+                        Gravity.Enabled = False
+                        C1Damage1.Enabled = False
+                        C1Damage2.Enabled = False
+                        C1Jump.Enabled = False
+
+                    End If
+                    ProgressBar1.Value = ProgressBar1.Value - 10
+                    C1Damage1.Enabled = True
+                End If
+            End If
+        End If
+
+        'character 2
+        If e.KeyChar = "l" Then
+            C2Foward.Enabled = True
+        End If
+        If e.KeyChar = "j" Then
+            C2Back.Enabled = True
+        End If
+
+    End Sub
+
+    Private Sub C1Jump_Tick(sender As Object, e As EventArgs) Handles C1Jump.Tick
+        Static x As Integer = 0
+        If x < 3 Then
+            C1P1.Top = C1P1.Top - 40
+            C1P2.Top = C1P1.Top
+            C1P3.Top = C1P1.Top
+            C1P4.Top = C1P1.Top
+            C1Punch.Top = C1P1.Top + 85
+            x = x + 1
+
+        Else
+            Gravity.Enabled = True
+            x = 0
+            C1Jump.Enabled = False
+
+        End If
+    End Sub
+
+    Private Sub C1Back_Tick(sender As Object, e As EventArgs) Handles C1Back.Tick
+        If C2P1.Left + C2P1.Width + 10 < C1P1.Left Then
+            C1Foward.Enabled = False
+            If C1P1.Left < 30 Then
+                C1Back.Enabled = False
+            Else
+                C1P1.Left = C1P1.Left - 30
+                C1P2.Left = C1P1.Left
+                C1P3.Left = C1P1.Left
+                C1P4.Left = C1P1.Left
+                Label3.Left = C1P1.Left - 50
+                Label3.Top = C1P1.Top - 70
+                C1Punch.Left = C1P1.Left
+                C1Back.Enabled = False
+            End If
+        End If
+
+
+    End Sub
+
+    Private Sub Gravity_Tick(sender As Object, e As EventArgs) Handles Gravity.Tick
+
+        Static x As Integer = 0
+        If x < 3 Then
+            C1P1.Top = C1P1.Top + 40
+            C1P2.Top = C1P1.Top
+            C1P3.Top = C1P1.Top
+            C1P4.Top = C1P1.Top
+            Label3.Left = C1P1.Left - 50
+            Label3.Top = C1P1.Top - 70
+            C1Punch.Top = C1P1.Top + 85
+            x = x + 1
+
+        Else
+            Gravity.Enabled = False
+            x = 0
+            BlnJump = True
+
+        End If
+    End Sub
+
+    Private Sub C1B1_Tick(sender As Object, e As EventArgs) Handles C1B1.Tick
+        C1P3.Visible = False
+        C1P4.Visible = True
+        C1P1.Visible = False
+        C1P2.Visible = False
+        C1B1.Enabled = False
+        C1B2.Enabled = True
+
+
+    End Sub
+
+    Private Sub C1B2_Tick(sender As Object, e As EventArgs) Handles C1B2.Tick
+        C1P3.Visible = True
+        C1P4.Visible = False
+        C1P1.Visible = False
+        C1P2.Visible = False
+        C1B2.Enabled = False
+        C1B1.Enabled = True
+    End Sub
+    Private Sub C1Bop1_Tick(sender As Object, e As EventArgs) Handles C1Bop1.Tick
+
+        C1P1.Visible = False
+        C1P2.Visible = True
+        C1P3.Visible = False
+        C1P4.Visible = False
+        C1Bop1.Enabled = False
+        C1Bop2.Enabled = True
+
+    End Sub
+
+    Private Sub C1Bop2_Tick(sender As Object, e As EventArgs) Handles C1Bop2.Tick
+        C1P1.Visible = True
+        C1P2.Visible = False
+        C1P3.Visible = False
+        C1P4.Visible = False
+        C1Bop1.Enabled = True
+        C1Bop2.Enabled = False
+
+    End Sub
+    'Character 2
+    Private Sub C2B1_Tick(sender As Object, e As EventArgs) Handles C2B1.Tick
+        C2P1.Visible = True
+        C2P2.Visible = False
+        C2B1.Enabled = False
+        C2B2.Enabled = True
+
+    End Sub
+
+    Private Sub C2B2_Tick(sender As Object, e As EventArgs) Handles C2B2.Tick
+        C2P1.Visible = False
+        C2P2.Visible = True
+        C2B1.Enabled = True
+        C2B2.Enabled = False
+    End Sub
+
+    Private Sub C2Foward_Tick(sender As Object, e As EventArgs) Handles C2Foward.Tick
+        If C1P1.Left > C2P1.Left + 293 Then
+            C1Foward.Enabled = False
+            If C2P1.Left > Me.Width - 200 Then
+                C2Foward.Enabled = False
+            Else
+                C2P1.Left = C2P1.Left + 50
+                C2P2.Left = C2P1.Left
+                C2Foward.Enabled = False
+            End If
+        End If
+
+
+    End Sub
+
+    Private Sub C2Back_Tick(sender As Object, e As EventArgs) Handles C2Back.Tick
+        If C2P1.Left < 30 Then
+            C2Back.Enabled = False
+        Else
+            C2P1.Left = C2P1.Left - 50
+            C2P2.Left = C2P1.Left
+            C2Back.Enabled = False
+        End If
+    End Sub
+
+    Private Sub C1Punch1_Tick(sender As Object, e As EventArgs) Handles C1Punch1.Tick
+        C1Punch.Visible = True
+        C1P2.Visible = False
+        C1P1.Visible = False
+        C1Punch1.Enabled = False
+        C1Punch2.Enabled = True
+
+
+    End Sub
+
+    Private Sub C1Punch2_Tick(sender As Object, e As EventArgs) Handles C1Punch2.Tick
+        C1Punch.Visible = False
+        C1P2.Visible = True
+        C1P1.Visible = False
+        C1Punch1.Enabled = False
+        C1Punch2.Enabled = False
+    End Sub
+
+    Private Sub C1Damage1_Tick(sender As Object, e As EventArgs) Handles C1Damage1.Tick
+        Label3.Visible = True
+        C1Damage2.Enabled = True
+        C1Damage1.Enabled = False
+    End Sub
+
+    Private Sub C1Damage2_Tick(sender As Object, e As EventArgs) Handles C1Damage2.Tick
+        Label3.Visible = False
+        C1Damage2.Enabled = False
+        C1Damage1.Enabled = False
+    End Sub
+End Class
